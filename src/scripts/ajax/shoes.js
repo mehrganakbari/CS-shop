3d672c105acf1b521ab3ad6865571f35c16105e4
MehrganAkbari <akbarimehrgan@yahoo.com>
2022-11-09 16:54:14 +0330

    shoes ajax completed

--- src/scripts/ajax/shoes.js
+++ src/scripts/ajax/shoes.js
@@ -1,111 +1,111 @@
 const shooesOne = {
-    title : "",
-    image : "",
-    price: "",
-    star: "",
-    seal: "",
+    title: "Nike Air white",
+    image: "../../assets/image/Screenshot_2022-10-17_185526-removebg-preview.png",
+    price: "40$",
+    star: 4.5,
+    seal: false,
 }
 
 const shooesTow = {
-    title : "",
-    image : "",
-    price: "",
-    star: "",
-    seal: "",
+    title: "Nike Air Black & White",
+    image: "../../assets/image/Screenshot_2022-10-17_185639-removebg-preview.png",
+    price: "45$",
+    star: 4,
+    seal: true,
 }
 
 const shooesThree = {
-    title : "",
-    image : "",
-    price: "",
-    star: "",
-    seal: "",
+    title: "Nike Sport black - SMA32",
+    image: "../../assets/image/Screenshot_2022-10-17_185712-removebg-preview.png",
+    price: "145$",
+    star: 5,
+    seal: false,
 }
 
 const shooesFour = {
-    title : "",
-    image : "",
-    price: "",
-    star: "",
-    seal: "",
+    title: "Nike Jordan-model long White",
+    image: "../../assets/image/Screenshot_2022-10-17_185408-removebg-preview.png",
+    price: "58$",
+    star: 3.5,
+    seal: true,
 }
 
 const shooesFive = {
-    title : "",
-    image : "",
-    price: "",
-    star: "",
-    seal: "",
+    title: "Nike Sport black - SMBA32",
+    image: "../../assets/image/Screenshot_2022-10-17_185742-removebg-preview.png",
+    price: "210$",
+    star: 4,
+    seal: true,
 }
 
 const shooesSix = {
-    title : "",
-    image : "",
-    price: "",
-    star: "",
-    seal: "",
+    title: "Nike Sport Black & White",
+    image: "../../assets/image/Screenshot_2022-10-17_184323-removebg-preview.png",
+    price: "49$",
+    star: 4,
+    seal: true,
 }
 
 const shooesSeven = {
-    title : "",
-    image : "",
-    price: "",
-    star: "",
-    seal: "",
+    title: "Rebook Sport Black",
+    image: "../../assets/image/Screenshot_2022-10-17_184557-removebg-preview.png",
+    price: "65$",
+    star: 4,
+    seal: false,
 }
 
 const shooesEight = {
-    title : "",
-    image : "",
-    price: "",
-    star: "",
-    seal: "",
+    title: "Adidas short - Blue & White",
+    image: "../../assets/image/Screenshot_2022-10-17_184427-removebg-preview.png",
+    price: "70$",
+    star: 4,
+    seal: false,
 }
 
-const shooesNein = {
-    title : "",
-    image : "",
-    price: "",
-    star: "",
-    seal: "",
+const shooesNine = {
+    title: "Nike short - Black & White",
+    image: "../../assets/image/Screenshot_2022-10-17_185817-removebg-preview.png",
+    price: "200$",
+    star: 4,
+    seal: false,
 }
 
 const shooesTen = {
-    title : "",
-    image : "",
-    price: "",
-    star: "",
-    seal: "",
+    title: "Rebook Air Black",
+    image: "../../assets/image/Screenshot_2022-10-17_184748-removebg-preview.png",
+    price: "23$",
+    star: 3,
+    seal: false,
 }
 
 const shooesEleven = {
-    title : "",
-    image : "",
-    price: "",
-    star: "",
-    seal: "",
+    title: "Nike Jordan-model short Red & Black",
+    image: "../../assets/image/Screenshot_2022-10-17_185447-removebg-preview.png",
+    price: "125$",
+    star: 5,
+    seal: false,
 }
 
 const shooesTwelve = {
-    title : "",
-    image : "",
-    price: "",
-    star: "",
-    seal: "",
+    title: "Under mora black ",
+    image: "../../assets/image/Screenshot_2022-10-17_184653-removebg-preview.png",
+    price: "80$",
+    star: 4.5,
+    seal: false,
 }
 
 const shooesThirteen = {
-    title : "",
-    image : "",
-    price: "",
-    star: "",
-    seal: "",
+    title: "Nike sport - SA35 Black & White",
+    image: "../../assets/image/Screenshot_2022-10-17_184507-removebg-preview.png",
+    price: "25$",
+    star: 3,
+    seal: true,
 }
 
 const shooesFourteen = {
-    title : "",
-    image : "",
-    price: "",
-    star: "",
-    seal: "",
+    title: "Adidas short - Gray & Black",
+    image: "../../assets/image/Screenshot_2022-10-17_185300-removebg-preview (2).png",
+    price: "30$",
+    star: 2.8,
+    seal: false,
 }
