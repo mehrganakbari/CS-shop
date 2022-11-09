a6505a2952e9775877e0da0644c2ed5febc650b5
MehrganAkbari <akbarimehrgan@yahoo.com>
2022-11-09 16:16:12 +0330

    shoes ajax template complated

--- src/scripts/ajax/shoes.js
+++ src/scripts/ajax/shoes.js
@@ -3,4 +3,109 @@ const shooesOne = {
     image : "",
     price: "",
     star: "",
+    seal: "",
+}
+
+const shooesTow = {
+    title : "",
+    image : "",
+    price: "",
+    star: "",
+    seal: "",
+}
+
+const shooesThree = {
+    title : "",
+    image : "",
+    price: "",
+    star: "",
+    seal: "",
+}
+
+const shooesFour = {
+    title : "",
+    image : "",
+    price: "",
+    star: "",
+    seal: "",
+}
+
+const shooesFive = {
+    title : "",
+    image : "",
+    price: "",
+    star: "",
+    seal: "",
+}
+
+const shooesSix = {
+    title : "",
+    image : "",
+    price: "",
+    star: "",
+    seal: "",
+}
+
+const shooesSeven = {
+    title : "",
+    image : "",
+    price: "",
+    star: "",
+    seal: "",
+}
+
+const shooesEight = {
+    title : "",
+    image : "",
+    price: "",
+    star: "",
+    seal: "",
+}
+
+const shooesNein = {
+    title : "",
+    image : "",
+    price: "",
+    star: "",
+    seal: "",
+}
+
+const shooesTen = {
+    title : "",
+    image : "",
+    price: "",
+    star: "",
+    seal: "",
+}
+
+const shooesEleven = {
+    title : "",
+    image : "",
+    price: "",
+    star: "",
+    seal: "",
+}
+
+const shooesTwelve = {
+    title : "",
+    image : "",
+    price: "",
+    star: "",
+    seal: "",
+}
+
+const shooesThirteen = {
+    title : "",
+    image : "",
+    price: "",
+    star: "",
+    seal: "",
+}
+
+const shooesFourteen = {
+    title : "",
+    image : "",
+    price: "",
+    star: "",
+    seal: "",
 }
