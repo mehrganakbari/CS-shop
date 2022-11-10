d2d5fe9cae32b250572a8a6cfd930a13b990c093
MehrganAkbari <akbarimehrgan@yahoo.com>
2022-11-10 16:28:36 +0330

    backpack ajax template complated

--- src/scripts/ajax/backpack.js
+++ src/scripts/ajax/backpack.js
@@ -1,6 +1,71 @@
 const backpackOne = {
-    title : "",
-    image : "",
-    price: "",
-    star: "",
+    title: "Adidas Rani",
+    image: "../../assets/image/Screenshot_2022-10-17_190207-removebg-preview.png",
+    price: "350$",
+    star: 4,
+    seal: false,
+}
+
+const backpackTow = {
+    title: "Under mora - Black",
+    image: "../../assets/image/Screenshot_2022-10-17_190317-removebg-preview.png",
+    price: "350$",
+    star: 4,
+    seal: true,
+}
+
+const backpackThree = {
+    title: "Jordan - sport black",
+    image: "../../assets/image/Screenshot_2022-10-17_190459-removebg-preview.png",
+    price: "900$",
+    star: 5,
+    seal: false,
+}
+
+const backpackFour = {
+    title: "Nike Sport - woman",
+    image: "../../assets/image/Screenshot_2022-10-17_190820-removebg-preview.png",
+    price: "100$",
+    star: 4.5,
+    seal: false,
+}
+
+const backpackFive = {
+    title: "Jordan Sport - Grey",
+    image: "../../assets/image/Screenshot_2022-10-17_191037-removebg-preview.png",
+    price: "210$",
+    star: 4,
+    seal: true,
+}
+
+const backpackSix = {
+    title: "Nike Modern - blue",
+    image: "../../assets/image/Screenshot_2022-10-17_191109-removebg-preview.png",
+    price: "310$",
+    star: 4,
+    seal: true,
+}
+
+const backpackSeven = {
+    title: "Nike Modern - black",
+    image: "../../assets/image/Screenshot_2022-10-17_191148-removebg-preview.png",
+    price: "120$",
+    star: 4,
+    seal: true,
+}
+
+const backpackEight = {
+    title: "Nike Modern - black-USA",
+    image: "../../assets/image/Screenshot_2022-10-17_191247-removebg-preview.png",
+    price: "430$",
+    star: 4,
+    seal: true,
+}
+
+const backpackNine = {
+    title: "Jack jons - red & black",
+    image: "../../assets/image/Screenshot_2022-10-17_191319-removebg-preview.png",
+    price: "500$",
+    star: 4,
+    seal: false,
 }
