f3d0f47710d6ec31020c211874e010e9f26d8cea
MehrganAkbari <akbarimehrgan@yahoo.com>
2022-11-10 15:44:58 +0330

    watch ajax completed

--- src/scripts/ajax/watch.js
+++ src/scripts/ajax/watch.js
@@ -1,6 +1,95 @@
 const watchOne = {
-    title : "",
-    image : "",
-    price: "",
-    star: "",
+    title: "Apple Watch 4",
+    image: "../../assets/image/",
+    price: "120$",
+    star: 5,
+    seal: false,
+}
+
+const watchTow = {
+    title: "Galaxy Watch 3",
+    image: "../../assets/image/",
+    price: "350$",
+    star: 4,
+    seal: false,
+}
+
+const watchThree = {
+    title: "Apple Watch 7",
+    image: "../../assets/image/",
+    price: "900$",
+    star: 5,
+    seal: false,
+}
+
+const watchFour = {
+    title: "Apple Watch 3",
+    image: "../../assets/image/",
+    price: "100$",
+    star: 4.5,
+    seal: false,
+}
+
+const watchFive = {
+    title: "Galaxy Watch 4",
+    image: "../../assets/image/",
+    price: "210$",
+    star: 4,
+    seal: false,
+}
+
+const watchSix = {
+    title: "MARQ",
+    image: "../../assets/image/",
+    price: "1120$",
+    star: 4,
+    seal: false,
+}
+
+const watchSeven = {
+    title: "Dorrati",
+    image: "../../assets/image/",
+    price: "120$",
+    star: 4,
+    seal: false,
+}
+
+const watchEight = {
+    title: "Omax silver - rose gold",
+    image: "../../assets/image/",
+    price: "430$",
+    star: 4,
+    seal: true,
+}
+
+const watchNine = {
+    title: "Rolex",
+    image: "../../assets/image/",
+    price: "500$",
+    star: 4,
+    seal: true,
+}
+
+const watchTen = {
+    title: "G-shake",
+    image: "../../assets/image/",
+    price: "700$",
+    star: 5,
+    seal: true,
+}
+
+const watchEleven = {
+    title: "Rolex woman",
+    image: "../../assets/image/",
+    price: "5420$",
+    star: 5,
+    seal: true,
+}
+
+const watchTwelve = {
+    title: "Bugatti",
+    image: "../../assets/image/",
+    price: "4300$",
+    star: 5,
+    seal: true,
 }
