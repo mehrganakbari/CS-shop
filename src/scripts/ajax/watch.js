cd5c35932beb973114797690afed4b18ce157a8c
MehrganAkbari <akbarimehrgan@yahoo.com>
2022-11-10 16:07:01 +0330

    edited watch ajax

--- src/scripts/ajax/watch.js
+++ src/scripts/ajax/watch.js
@@ -1,6 +1,6 @@
 const watchOne = {
     title: "Apple Watch 4",
-    image: "../../assets/image/",
+    image: "../../assets/image/Screenshot_2022-10-18_172735-removebg-preview.png",
     price: "120$",
     star: 5,
     seal: false,
@@ -8,15 +8,15 @@ const watchOne = {
 
 const watchTow = {
     title: "Galaxy Watch 3",
-    image: "../../assets/image/",
+    image: "../../assets/image/Screenshot_2022-10-18_172826-removebg-preview.png",
     price: "350$",
     star: 4,
-    seal: false,
+    seal: true,
 }
 
 const watchThree = {
     title: "Apple Watch 7",
-    image: "../../assets/image/",
+    image: "../../assets/image/Screenshot_2022-10-18_172902-removebg-preview.png",
     price: "900$",
     star: 5,
     seal: false,
@@ -24,7 +24,7 @@ const watchThree = {
 
 const watchFour = {
     title: "Apple Watch 3",
-    image: "../../assets/image/",
+    image: "../../assets/image/Screenshot_2022-10-18_173120-removebg-preview.png",
     price: "100$",
     star: 4.5,
     seal: false,
@@ -32,7 +32,7 @@ const watchFour = {
 
 const watchFive = {
     title: "Galaxy Watch 4",
-    image: "../../assets/image/",
+    image: "../../assets/image/Screenshot_2022-10-18_173312-removebg-preview.png",
     price: "210$",
     star: 4,
     seal: false,
@@ -40,7 +40,7 @@ const watchFive = {
 
 const watchSix = {
     title: "MARQ",
-    image: "../../assets/image/",
+    image: "../../assets/image/Screenshot_2022-10-18_173247-removebg-preview.png",
     price: "1120$",
     star: 4,
     seal: false,
@@ -48,7 +48,7 @@ const watchSix = {
 
 const watchSeven = {
     title: "Dorrati",
-    image: "../../assets/image/",
+    image: "../../assets/image/Screenshot_2022-10-18_173054-removebg-preview.png",
     price: "120$",
     star: 4,
     seal: false,
@@ -56,7 +56,7 @@ const watchSeven = {
 
 const watchEight = {
     title: "Omax silver - rose gold",
-    image: "../../assets/image/",
+    image: "../../assets/image/Screenshot_2022-10-18_173346-removebg-preview.png",
     price: "430$",
     star: 4,
     seal: true,
@@ -64,7 +64,7 @@ const watchEight = {
 
 const watchNine = {
     title: "Rolex",
-    image: "../../assets/image/",
+    image: "../../assets/image/Screenshot_2022-10-18_173540-removebg-preview.png",
     price: "500$",
     star: 4,
     seal: true,
@@ -72,7 +72,7 @@ const watchNine = {
 
 const watchTen = {
     title: "G-shake",
-    image: "../../assets/image/",
+    image: "../../assets/image/Screenshot_2022-10-18_172955-removebg-preview.png",
     price: "700$",
     star: 5,
     seal: true,
@@ -80,7 +80,7 @@ const watchTen = {
 
 const watchEleven = {
     title: "Rolex woman",
-    image: "../../assets/image/",
+    image: "../../assets/image/Screenshot_2022-10-18_173447-removebg-preview.png",
     price: "5420$",
     star: 5,
     seal: true,
@@ -88,8 +88,8 @@ const watchEleven = {
 
 const watchTwelve = {
     title: "Bugatti",
-    image: "../../assets/image/",
+    image: "../../assets/image/Screenshot_2022-10-18_173625-removebg-preview.png",
     price: "4300$",
     star: 5,
-    seal: true,
+    seal: false,
 }
