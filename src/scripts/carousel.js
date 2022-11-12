e14f2bffcec16db8cf0c162b88a7ef2b3bc6e143
MehrganAkbari <akbarimehrgan@yahoo.com>
2022-11-12 16:42:57 +0330

    added carousel.js (carousel options file)

---
+++ src/scripts/carousel.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    $('.owl-carousel').owlCarousel({
+      rtl: false,
+      margin: 10,
+      nav: true,
+      responsive: {
+        0: {
+          items: 1
+        },
+        800: {
+          items: 1
+        },
+        1000: {
+          items: 1
+        }
+      }
+    })
+  });
