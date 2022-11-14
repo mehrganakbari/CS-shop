680bc1f73494961a9d1d3ddbf423e039e7de85fc
MehrganAkbari <akbarimehrgan@yahoo.com>
2022-11-14 10:22:20 +0330

    added AboutUs in vite config

--- vite.config.js
+++ vite.config.js
@@ -13,6 +13,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
+        about: resolve(__dirname, "AboutUs.html"),
       },
     },
   },
