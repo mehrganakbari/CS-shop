3b208b116ec5c6c6b99b885743fbf4a022158daf
mehrgan akbari <akbarimehrgan@yahoo.com>
2022-11-22 22:39:07 -0800

    edited home page & created using json template

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,13 @@
 import "../common/index.css"
+// import shoesProducts from "json/shoes.json"
+// import backpackProducts from "json/shoes.json"
+// import clothProducts from "json/shoes.json"
+// import watchProducts from "json/shoes.json"
+
+ const OfferBoxWatch = querySelector("offerbywatchSect");
+ const addOffer = (title, image , price, star, seal) => {
+    OfferBoxWatch.InnerHTML += ``;
+ }
 
 const btn = document.querySelector("button.mobile-menu-button");
 const menu = document.querySelector(".mobile-menu");
@@ -6,3 +15,4 @@ const menu = document.querySelector(".mobile-menu");
 btn.addEventListener("click", () => {
     menu.classList.toggle("hidden");
 });
+
