143d35908aa719ab45e2a82c1ce61a282e8ba9e0
mehrgan akbari <akbarimehrgan@yahoo.com>
2022-11-27 11:26:37 +0330

    bug up -> main.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,18 +1,44 @@
 import "../common/index.css"
-// import shoesProducts from "json/shoes.json"
-// import backpackProducts from "json/shoes.json"
-// import clothProducts from "json/shoes.json"
-// import watchProducts from "json/shoes.json"
-
- const OfferBoxWatch = querySelector("offerbywatchSect");
- const addOffer = (title, image , price, star, seal) => {
-    OfferBoxWatch.InnerHTML += ``;
- }
 
 const btn = document.querySelector("button.mobile-menu-button");
 const menu = document.querySelector(".mobile-menu");
 
 btn.addEventListener("click", () => {
-    menu.classList.toggle("hidden");
+  menu.classList.toggle("hidden");
 });
 
+const OfferBoxWatch = document.querySelector("#offerbywatchSect");
+
+const addwatch = (WatchProduct) => {
+  OfferBoxWatch.InnerHTML += `
+  <div class="w-72 m-3">
+  <div class="bg-lite-blue border border-lite-gray flex justify-center relative w-72 h-72 items-center">
+    <div class="flex justify-center"><img src="${WatchProduct.image}"
+        class="w-52"></div>
+    <div class="flex justify-between flex-col absolute bottom-0 left-2">
+      <img src="assets/images/like.png" class="py-1 cursor-pointer">
+      <img src="assets/images/favorite.png" class="py-1 cursor-pointer">
+      <img src="assets/images/shoppingcart.png" class="py-1 cursor-pointer">
+    </div>
+  </div>
+  <div class="text-center text-lite">
+    <p class="py-2 text-3xl">${WatchProduct.title}</p>
+    <p class="py-1 text-xl">${WatchProduct.price}</p>
+      <button class="py-2 px-5 text-dark bg-lite-blue rounded-full">read more >></button>
+      <div class="flex justify-center py-2">
+        <span class="pr-1">${WatchProduct.star}</span>
+        <img src="assets/images/star.png">
+      </div>
+  </div>
+</div>
+  `;
+};
+
+fetch("./watch.json")
+  .then((res) => res.json() )
+  .then((data) => {
+    console.log(data);
+    data.map((WatchProduct) => {
+      addwatch(WatchProduct);
+    });
+});
--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
-        about: resolve(__dirname, "AboutUs.html"),
+        nested: resolve(__dirname, "AboutUs.html")
       },
     },
   },
